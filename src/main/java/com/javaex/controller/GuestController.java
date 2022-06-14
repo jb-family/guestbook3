7038411151c493e673fbe4039ff179f51ca132c1
jongbin-choi <kooma1123@naver.com>
2022-06-14 17:19:09 +0900

    guestbook3 spring

--- src/main/java/com/javaex/controller/GuestController.java
+++ src/main/java/com/javaex/controller/GuestController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.javaex.dao.GuestBookDao;
 import com.javaex.service.GuestService;
 import com.javaex.vo.GuestBookVo;
 
